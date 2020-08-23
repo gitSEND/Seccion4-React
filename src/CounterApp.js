1a012697031d72b8fe82f16510689e54b92d834a
Nelson <n.sanchezesparza@gmail.com>
2020-08-22 22:05:19 -0500

    Hook => useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 // snipets => rafc: crea las 2 importaciones
 
 const CounterApp = ({ value }) => {
-  const handleAdd = (e) => {
-    console.log(e);
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => {
+    // setCounter(counter + 1); // 1er forma
+    setCounter((c) => c + 1); // 2da forma
   };
 
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       <button onClick={handleAdd}> +1 </button>
     </>
   );
