30667c12de5246de85c8fc44a22fadec5074c4c5
Nelson <n.sanchezesparza@gmail.com>
2020-08-22 20:50:27 -0500

    PropTypes & propTypes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,14 +1,18 @@
 // Functional Component
-// import React, { Fragment } from "react";
 import React from "react";
+import PropTypes from "prop-types";
 
-const PrimeraApp = ({ saludo = "Hola mundo" }) => {
+const PrimeraApp = ({ saludo }) => {
   return (
     <>
       <h1>{saludo}</h1>
       <p>Mi primera aplicacion</p>
     </>
   );
+};
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
 };
 
 export default PrimeraApp;
