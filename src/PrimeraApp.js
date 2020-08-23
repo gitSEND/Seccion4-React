4b82a985a25c358f2c0f43e78668d7cdb4dc81da
Nelson <n.sanchezesparza@gmail.com>
2020-08-22 20:44:06 -0500

    Comunicacion entre componentes props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,15 +2,11 @@
 // import React, { Fragment } from "react";
 import React from "react";
 
-const PrimeraApp = () => {
+const PrimeraApp = ({ saludo = "Hola mundo" }) => {
   return (
-    // <Fragment>
-    //   <h1>Hola mundo</h1>
-    //   <h1>Hola mundo</h1>
-    // </Fragment>
     <>
-      <h1>Hola mundo</h1>
-      <h1>Hola mundo</h1>
+      <h1>{saludo}</h1>
+      <p>Mi primera aplicacion</p>
     </>
   );
 };
